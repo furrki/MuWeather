20baae2d26719d7c5c7f1800ad4a3877aaab025f
furrki-Mac <admin@Admins-Mac.local>
2019-03-15 22:55:32 +0300

    Folder Structure set

--- MuWeather/Controller/MainVC.swift
+++ MuWeather/Controller/MainVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainVC.swift
 //  MuWeather
 //
 //  Created by Admin on 15.03.2019.
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class MainVC: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
