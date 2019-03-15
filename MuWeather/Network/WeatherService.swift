5432840dde8cb2ebac27f16231b00533d19ecdba
furrki-Mac <admin@Admins-Mac.local>
2019-03-15 22:57:31 +0300

    WeatherService created

---
+++ MuWeather/Network/WeatherService.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherService.swift
+//  MuWeather
+//
+//  Created by Admin on 15.03.2019.
+//  Copyright © 2019 furrki. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class WeatherService {
+    
+}
