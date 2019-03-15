bc4113e241008c821aa0895e63af400525cea4bd
furrki-Mac <admin@Admins-Mac.local>
2019-03-15 23:46:07 +0300

    Service class created

--- MuWeather/Network/WeatherService.swift
+++ MuWeather/Network/WeatherService.swift
@@ -10,5 +10,16 @@ import Foundation
 import Alamofire
 
 class WeatherService {
+    static var shared = WeatherService()
     
+    func getUrlFor(woeid: String) -> String {
+        return "\(apiURL)/\(woeid)"
+    }
+    
+    func getUrlFor(woeid: String, date: Date) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "d/MM/yyyy"
+        let dateUrlPart = dateFormatter.string(from: date)
+        return "\(apiURL)/\(woeid)/\(dateUrlPart)"
+    }
 }
---
+++ MuWeather/Util/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  MuWeather
+//
+//  Created by Admin on 15.03.2019.
+//  Copyright © 2019 furrki. All rights reserved.
+//
+
+import Foundation
+
+var apiURL = "https://www.metaweather.com/api/location"
