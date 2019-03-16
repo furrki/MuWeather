7a598bc93de28e7785e5052d92b2452c5c7afde4
furrki-Mac <admin@Admins-Mac.local>
2019-03-16 19:39:47 +0300

    Added Weather Model

---
+++ MuWeather/Model/Weather.swift
@@ -0,0 +1,46 @@
+//
+//  Weather.swift
+//  MuWeather
+//
+//  Created by Admin on 16.03.2019.
+//  Copyright © 2019 furrki. All rights reserved.
+//
+
+import Foundation
+
+class Weather {
+    var description: String
+    var windSpeed: Double
+    var temperature: Int
+    var humidity: Int
+    var pressure: Int
+    var date: Date
+    var iconString: String
+    var locationDescription = ""
+    
+    var windDirection: String
+    var longDescription: String {
+        return "\(description) \(temperature) \(pressure) \(humidity) \(date) \(windDirection) \(windSpeed) "
+    }
+    
+    var dateString: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy - HH:mm"
+        return dateFormatter.string(from: date)
+    }
+    
+    init(description: String, windSpeed: Double, windDirection: String, temperature: Int, humidity: Int, pressure: Int, date: Date, iconString: String, city: String? = nil) {
+        self.description = description
+        self.windSpeed = windSpeed
+        self.windDirection = windDirection
+        self.temperature = temperature
+        self.humidity = humidity
+        self.pressure = pressure
+        self.date = date
+        self.iconString = iconString
+        if city != "" {
+            self.locationDescription = "\(city ?? "")"
+        }
+    }
+
+}
